a0a74e092c45f8e577b44bf19190b66041b122c0
Matheus Torres dos Reis <matheus.torres97@outlook.com>
2025-09-25 10:31:51 -0300

    delete and update features

---
+++ src/Services/Catalog/Catalog.API/Products/Delete/DeleteProductCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.Delete;
+
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+
+public record DeleteProductResult(bool IsSuccess);
+
+public class DeleteProductCommandHandler(
+    IDocumentSession session,
+    ILogger<DeleteProductCommandHandler> logger) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Command}", command);
+
+        session.Delete<Product>(command.Id);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new DeleteProductResult(true);
+    }
+}
