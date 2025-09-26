33c456fc9f74ef1ed1eb8cf7ab7bb9410aa957ad
Matheus Torres dos Reis <matheus.torres97@outlook.com>
2025-09-26 12:31:41 -0300

    added pagination using marten library

--- src/Services/Catalog/Catalog.API/Products/Get/GetProductsQueryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/Get/GetProductsQueryHandler.cs
@@ -1,10 +1,11 @@
 ﻿using BuildingBlocks.CQRS;
 using Catalog.API.Models;
 using Marten;
+using Marten.Pagination;
 
 namespace Catalog.API.Products.Get;
 
-public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsQuery(int? Page = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
 public record GetProductsResult(IEnumerable<Product> Products);
 public sealed class GetProductsQueryHandler(
     IDocumentSession session) : IQueryHandler<GetProductsQuery, GetProductsResult>
@@ -13,7 +14,7 @@ public sealed class GetProductsQueryHandler(
     {
         var produts = await session
             .Query<Product>()
-            .ToListAsync(cancellationToken);
+            .ToPagedListAsync(query.Page ?? 1, query.PageSize ?? 10, cancellationToken);
 
         return new GetProductsResult(produts);
     }
