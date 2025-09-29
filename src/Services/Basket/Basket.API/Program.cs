7ba3a6d7038a8aaa0c84c0bfb35d69248dfc381f
Matheus Torres dos Reis <matheus.torres97@outlook.com>
2025-09-29 14:52:12 -0300

    added healthcheck

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -3,7 +3,9 @@ using Basket.API.Models;
 using BuildingBlocks.Behaviors;
 using BuildingBlocks.Exceptions.Handlers;
 using Carter;
+using HealthChecks.UI.Client;
 using Marten;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,26 +17,39 @@ builder.Services.AddMediatR(x =>
     x.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 
+var databaseConnectionString = builder.Configuration.GetConnectionString("Database")!;
+
 builder.Services.AddMarten(opts =>
 {
-    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Connection(databaseConnectionString);
     opts.Schema.For<ShoppingCart>().Identity(x => x.Username); // TODO: APÓS O CURSO MUDAR PARA USAR ID AO INVÉS DE USERNAME 
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
 
+var redisConnectionString = builder.Configuration.GetConnectionString("Redis")!;
+
 builder.Services.AddStackExchangeRedisCache(options =>
 {
-    options.Configuration = builder.Configuration.GetConnectionString("redis");
+    options.Configuration = redisConnectionString;
 });
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(databaseConnectionString)
+    .AddRedis(redisConnectionString);
 
 var app = builder.Build();
 
 app.MapCarter();
 
 app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
